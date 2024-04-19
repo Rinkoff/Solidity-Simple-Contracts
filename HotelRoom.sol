3e2e4dc7a3e724e58a5c1fde73302e374cb2c96c
Rinkoff <122561323+Rinkoff@users.noreply.github.com>
2024-04-20 01:12:51 +0300

    Add files via upload

---
+++ HotelRoom.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+contract HotelRoom {
+
+    address payable public owner;
+
+    enum roomStatus {Vacant, Occupied}
+
+    roomStatus public currentStatus;
+
+    event Occupy(address _occupier, uint _value);
+
+    constructor(){
+        owner = payable(msg.sender);
+        currentStatus = roomStatus.Vacant;
+    }
+
+    modifier onlyWhileVacant{
+        require(currentStatus == roomStatus.Vacant,"Currently occupied.");
+        _;
+    }
+
+    modifier costs (uint _amount) {
+        require(msg.value >= _amount,"Not enough Ether provided.");
+        _;
+    }
+
+    function bookRoom () public payable onlyWhileVacant costs(2 ether) {
+
+        (bool sent, bytes memory data) = owner.call{value: msg.value}("");
+        require(true);
+
+        currentStatus = roomStatus.Occupied;
+        emit Occupy(msg.sender,msg.value);
+
+
+
+    }
+
+
+
+
+}
